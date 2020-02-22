80f06c7e490246fae626b629718df06daaa9f438
Anu Bansal <anubansal9999@gmail.com>
2020-02-22 18:53:29 +0530

    Create StringConcatenation.java

---
+++ Strings/StringConcatenation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.println("first string");
+		String str1 = sc.next();
+		System.out.println("Second string");
+        String str2 = sc.next();
+        System.out.println(str1 + " " + str2);
+	}
+
+}
