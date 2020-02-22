78a14f43cf2008b66fb5137c2fa6c179b3566bde
Anu Bansal <anubansal9999@gmail.com>
2020-02-22 23:19:05 +0530

    Create RemoveAllOccurencesOfAChar.java

---
+++ Strings/RemoveAllOccurencesOfAChar.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Strings {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		char c = sc.next().charAt(0);
+		int length = str.length();
+		int j=0;
+		int count = 0;
+		char[] t = str.toCharArray();
+		for(int i=0; i<length; i++)
+		{
+			if(t[i]!=c)
+				t[j++] = t[i];
+			else
+				count++;
+			
+		}
+		while(count>0)
+		{
+			t[j++] = '\0';
+			count--;
+		}
+		System.out.print(t);
+ 	}
+	
+
+}
